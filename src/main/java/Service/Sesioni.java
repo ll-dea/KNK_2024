bac18fe1d3ce0979d78843998542d9de70785539
ElisaGjurkaj <elisa.gjrk@gmail.com>
2024-05-21 19:12:17 +0200

    Krijimi Sesioneve

---
+++ src/main/java/Service/Sesioni.java
@@ -0,0 +1,21 @@
+package Service;
+
+import Model.OrariKonsultimeve;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Sesioni {
+    private List<OrariKonsultimeve> oraret;
+
+    public Sesioni() {
+        oraret = new ArrayList<>();
+    }
+
+    public void shtoOrar(OrariKonsultimeve orari) {
+        oraret.add(orari);
+    }
+
+    public List<OrariKonsultimeve> getOraret() {
+        return oraret;
+    }
+}
