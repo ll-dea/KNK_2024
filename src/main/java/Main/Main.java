8026a095de7e1d1e2457d022d0760f4339ab91fb
DeaLimoni <dea.limoni@student.uni-pr.edu>
2024-05-22 16:00:56 +0200

    Fixed home

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/sceneBuilderFiles/login.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/sceneBuilderFiles/Signup.fxml"));
         stage.initStyle(StageStyle.UNDECORATED);
         Scene scene = new Scene(fxmlLoader.load(), 700, 500);
         stage.setScene(scene);
